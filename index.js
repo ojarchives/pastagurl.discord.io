525834cf8b331fbe104a93acada4db6351cc80b3
JPArchives <127899997+ojarchives@users.noreply.github.com>
2023-08-29 22:33:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1015397924882087986&permissions=8&scope=bot"
+window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1015397924882087986&permissions=8&scope=bot%20applications.commands"
